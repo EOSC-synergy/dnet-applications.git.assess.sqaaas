23a793e3353f1ee0ab7de93ca15085ce69d87166
orviz <orviz@ifca.unican.es>
2023-04-11 15:32:40 +0200

    Add JePL file structure

---
+++ .sqa/script.cheeky-gamboge-russel.sh
@@ -0,0 +1,4 @@
+(
+cd code-repo.d4science.org/D-Net/dnet-applications &&
+    git rev-parse --is-inside-work-tree
+)
---
+++ .sqa/script.squirrely-indigo-grouse.sh
@@ -0,0 +1,4 @@
+(
+cd code-repo.d4science.org/D-Net/dnet-applications &&
+    licensee detect . --confidence 60 --json
+)
